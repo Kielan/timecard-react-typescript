d67ac8d4a92a024ad653e2d090c8e737a7a15367
kielan <ki.lemons@gmail.com>
2021-09-24 12:25:32 -0700

    remove optional type before interfaces written

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,7 +97,7 @@ export default function Home() {
 			<AppHeader />
       <HomeMain>
 				<NextShift />
-        <Calendar scheduledShifts={scheduledShifts} />
+        <Calendar />
       </HomeMain>
     </HomeWrapper>
   )
